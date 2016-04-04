00725b30f95ae2045e2a73584ff47146329e7bcd
microsheep <twmicrosheep@gmail.com>
2016-04-04 21:15:47 +0800

    update hw2

---
+++ HW2/hw2.cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <msgpack.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+int main(void) {
+    FILE *fin, *fout;
+
+    struct stat st;
+    stat("./input.txt", &st);
+    char* buf = new char[st.st_size];
+
+    fin = fopen("input.txt", "rb");
+    fread(buf, st.st_size, 1, fin);
+    fclose(fin);
+    
+    size_t off=0;
+    msgpack::unpacked result;
+    msgpack::unpack(result, buf, st.st_size, off);
+    
+    int num_tests;
+    result.get().convert(num_tests);
+
+    msgpack::sbuffer sbuf;
+    vector<int> NUMBERS;
+    for(int k=0; k<num_tests; k++){
+        msgpack::unpack(result, buf, st.st_size, off);
+        result.get().convert(NUMBERS);
+        sort(NUMBERS.begin(), NUMBERS.end());
+        msgpack::pack(&sbuf, NUMBERS);
+    }
+    
+    delete[] buf;
+
+    fout = fopen("output.txt", "wb");
+    fwrite(sbuf.data(), sbuf.size(), 1, fout);
+    fclose(fout);
+}
+
+
