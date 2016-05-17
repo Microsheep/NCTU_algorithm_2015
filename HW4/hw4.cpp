ab84fafea6534b2680d0a162e8a21a8ba68151a7
microsheep <twmicrosheep@gmail.com>
2016-05-17 19:21:48 +0800

    speed up

--- HW4/hw4.cpp
+++ HW4/hw4.cpp
@@ -6,14 +6,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #define MAX_node 200000
-#define NEGINF -999999999ll
 using namespace std;
 
 struct node{
-    long long my_sum=0;
-    long long my_max=0;
-    long long l_max=0;
-    long long r_max=0; 
+    long long my_sum;
+    long long my_max;
+    long long l_max;
+    long long r_max; 
 };
 
 node seg_tree[MAX_node];
@@ -33,9 +32,8 @@ void build_tree(int x, int start, int end){
         seg_tree[x].my_sum=seg_tree[2*x].my_sum+seg_tree[2*x+1].my_sum;
         seg_tree[x].l_max=max(seg_tree[2*x].my_sum+seg_tree[2*x+1].l_max,seg_tree[2*x].l_max);
         seg_tree[x].r_max=max(seg_tree[2*x].r_max+seg_tree[2*x+1].my_sum,seg_tree[2*x+1].r_max);
-        seg_tree[x].my_max=max(seg_tree[x].my_sum,max(seg_tree[2*x].r_max+seg_tree[2*x+1].l_max,max(seg_tree[2*x].my_max,seg_tree[2*x+1].my_max)));
+        seg_tree[x].my_max=max(seg_tree[2*x].r_max+seg_tree[2*x+1].l_max,max(seg_tree[2*x].my_max,seg_tree[2*x+1].my_max));
     }
-    return;
 }
 
 node seg_q(int x, int start, int end, int a, int b){
@@ -56,7 +54,7 @@ node seg_q(int x, int start, int end, int a, int b){
         tmp.my_sum=n1.my_sum+n2.my_sum;
         tmp.l_max=max(n1.l_max,n1.my_sum+n2.l_max);
         tmp.r_max=max(n2.r_max,n2.my_sum+n1.r_max);
-        tmp.my_max=max(n1.my_sum+n2.my_sum,max(n1.r_max+n2.l_max,max(n1.my_max,n2.my_max)));
+        tmp.my_max=max(n1.r_max+n2.l_max,max(n1.my_max,n2.my_max));
         return tmp;
     }
 }
@@ -83,13 +81,11 @@ int main(void) {
     
     while(num_tests--){
         int item_num;
-        items.clear();
         msgpack::unpack(result, buf, st.st_size, off);
         result.get().convert(item_num);
         msgpack::unpack(result, buf, st.st_size, off);
         result.get().convert(items);
         
-        memset(seg_tree,0,sizeof(seg_tree)); 
         build_tree(1,1,item_num);
 
         vector<long long> ans;
@@ -97,15 +93,16 @@ int main(void) {
         msgpack::unpack(result, buf, st.st_size, off);
         result.get().convert(query_num);
         
-        for(int q=0;q<query_num;q++){
+        while(query_num--){
             vector<int> query;
             msgpack::unpack(result, buf, st.st_size, off);
             result.get().convert(query);
-
-            node tmp_ans=seg_q(1, 1, item_num, query[0], query[1]);
-            ans.push_back(tmp_ans.my_max);
+            ans.push_back(seg_q(1, 1, item_num, query[0], query[1]).my_max);
         } 
         msgpack::pack(&sbuf, ans);
+        
+        items.clear();
+        memset(seg_tree,0,4*sizeof(node)*item_num); 
     }
     
     delete[] buf;
